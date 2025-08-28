ea71a258429881e4b14655db15e4c6faece98bbc
hwangjiyoung02 <hjy020714@gmail.com>
2025-08-29 00:45:51 +0900

    [setting/#1] 초기 Spring Boot 프로젝트 세팅

---
+++ src/main/java/com/goormthon/backend/firstsori/FirstSoriApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.backend.firstsori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSoriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goormthon/backend/firstsori/FirstSoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.backend.firstsori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
