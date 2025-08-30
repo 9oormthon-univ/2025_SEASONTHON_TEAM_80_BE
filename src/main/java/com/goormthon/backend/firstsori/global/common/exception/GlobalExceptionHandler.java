53ee35496c9f14aad1b550f8aa69347f70534eaa
Choi Min Young <codingmy@naver.com>
2025-08-31 00:04:05 +0900

    [refactor/#7] 필드 변경 반영

--- src/main/java/com/goormthon/backend/firstsori/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/goormthon/backend/firstsori/global/common/exception/GlobalExceptionHandler.java
@@ -37,7 +37,7 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = ErrorCode.fromMessage(e.getMessage());
 
         /// 해당 예외 코드로 예외 처리
-        CustomException exception = new CustomException(errorCode, null);
+        CustomException exception = new CustomException(errorCode.getHttpStatus(), errorCode.getCode(), errorCode.getMessage());
 
         return handleCustomException(exception);
     }
@@ -50,7 +50,7 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = ErrorCode.fromMessage(e.getMessage());
 
         /// 해당 예외 코드로 예외 처리
-        CustomException exception = new CustomException(errorCode, null);
+        CustomException exception = new CustomException(errorCode.getHttpStatus(), errorCode.getCode(), errorCode.getMessage());
 
         return handleCustomException(exception);
     }
@@ -67,7 +67,7 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
 
         /// 해당 예외 코드로 예외 처리
-        CustomException exception = new CustomException(errorCode, null);
+        CustomException exception = new CustomException(errorCode.getHttpStatus(), errorCode.getCode(), errorCode.getMessage());
 
         return handleCustomException(exception);
     }
