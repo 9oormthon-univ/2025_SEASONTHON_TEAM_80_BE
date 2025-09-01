ac120d600b4ea2f86633427a6c2971d1ff4ba922
hwangjiyoung02 <hjy020714@gmail.com>
2025-09-01 16:30:42 +0900

    [refactor/#5] BoardControllerSpec 수정

--- src/main/java/com/goormthon/backend/firstsori/domain/board/presentation/spec/BoardControllerSpec.java
+++ src/main/java/com/goormthon/backend/firstsori/domain/board/presentation/spec/BoardControllerSpec.java
@@ -26,19 +26,42 @@ public interface BoardControllerSpec {
     })
     @GetMapping("/{messageId}")
     com.goormthon.backend.firstsori.global.common.response.ApiResponse<MessageResponse> getMessage(
-            @Parameter(description = "조회할 메세지 ID") @PathVariable UUID messageId
-    );
+            @Parameter(
+                    description = "조회할 메세지 ID",
+                    example = "ff54b002-7f36-496e-9d8e-cf7dc41556d7"
+            )
+            @PathVariable UUID messageId    );
 
-    @Operation(summary = "전체 메세지 목록 조회", description = "사용자 ID에 해당하는 게시판의 메세지 목록을 페이징하여 조회합니다.")
+    @Operation(
+            summary = "전체 메세지 목록 조회",
+            description = "사용자 ID에 해당하는 게시판의 메세지 목록을 페이징하여 조회합니다."
+    )
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK",
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
                     content = @io.swagger.v3.oas.annotations.media.Content(
-                            mediaType = "application/json")),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "사용자 또는 게시판을 찾을 수 없음")
+                            mediaType = "application/json"
+                    )
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "404",
+                    description = "사용자 또는 게시판을 찾을 수 없음"
+            )
     })
     @GetMapping
     com.goormthon.backend.firstsori.global.common.response.ApiResponse<PageResponse<MessageListResponse>> getMessages(
-            @Parameter(description = "메세지를 조회할 사용자 ID") @RequestParam UUID userId,
+            @Parameter(
+                    description = "메세지를 조회할 사용자 ID",
+                    example = "fd1c199e-ae84-4cbd-9baa-e94da3d553d0"
+            )
+            @RequestParam UUID userId,
+
+            @Parameter(
+                    description = "페이징 정보",
+                    example = "{ \"page\": 0, \"size\": 10, \"sort\": [\"createdDate,desc\"] }"
+            )
             Pageable pageable
     );
+
 }
