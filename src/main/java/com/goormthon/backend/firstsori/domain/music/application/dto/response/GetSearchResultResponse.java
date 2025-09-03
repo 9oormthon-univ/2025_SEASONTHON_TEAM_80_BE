33d2a0ce10da3979d938db518c8ae3375b2e0f00
Choi Min Young <codingmy@naver.com>
2025-09-03 20:24:17 +0900

    [docs/#13] response dto에 Swagger annotation 추가

--- src/main/java/com/goormthon/backend/firstsori/domain/music/application/dto/response/GetSearchResultResponse.java
+++ src/main/java/com/goormthon/backend/firstsori/domain/music/application/dto/response/GetSearchResultResponse.java
@@ -1,8 +1,14 @@
 package com.goormthon.backend.firstsori.domain.music.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
+// 음악 검색 결과 DTO
+@Schema(name = "[GET] 음악 검색 response",
+        description = "음악 검색 결과 응답")
 public record GetSearchResultResponse(
+
+        @Schema(description = "검색된 곡 정보 목록")
         List<SongData> searchResult
 ) {
 }
--- src/main/java/com/goormthon/backend/firstsori/domain/music/application/dto/response/SongData.java
+++ src/main/java/com/goormthon/backend/firstsori/domain/music/application/dto/response/SongData.java
@@ -1,11 +1,27 @@
 package com.goormthon.backend.firstsori.domain.music.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+// 음악 메타데이터 DTO
+@Schema(description = "음악 메타데이터 응답")
 public record SongData(
+
+        @Schema(description = "곡 고유 ID")
         String songId,
+
+        @Schema(description = "곡 제목")
         String songTitle,
+
+        @Schema(description = "아티스트명 (여러 명일 경우 콤마 구분)")
         String artist,
+
+        @Schema(description = "커버 이미지 URL (앨범 이미지)")
         String coverImage,
+
+        @Schema(description = "곡 스트리밍 URL (Spotify 등 외부 링크)")
         String streamingUrl,
+
+        @Schema(description = "미리 듣기 URL (30초 미리듣기 등)")
         String prestreamingUrl
 ) {
 }
