e6072ef0468ebc46869ac23827c662bad5b8f8ce
hwangjiyoung02 <hjy020714@gmail.com>
2025-09-06 18:34:24 +0900

    [fix/#55] music 중복 저장 방지 로직 추가

--- src/main/java/com/goormthon/backend/firstsori/domain/music/domain/service/SaveMusicService.java
+++ src/main/java/com/goormthon/backend/firstsori/domain/music/domain/service/SaveMusicService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -14,10 +16,20 @@ public class SaveMusicService {
     private final MusicRepository musicRepository;
     private final MusicEventProducer musicEventProducer;
 
-    public void saveMusic(Music music) {
-        // DB에 음악 저장
-        Music savedMusic = musicRepository.save(music);
-        // 인기 차트 이벤트 발행
-        musicEventProducer.publishMusicEvent(savedMusic.getSongName());
+    public Music saveMusic(Music music) {
+        Optional<Music> existsMusic = musicRepository.findBySongNameAndArtist(music.getSongName(), music.getArtist());
+        if (existsMusic.isEmpty()) { // DB에 없으면 저장
+            Music savedMusic = musicRepository.save(music);
+            musicEventProducer.publishMusicEvent(savedMusic.getSongName());
+            return savedMusic;
+
+        } else { // 이미 있으면 저장 안하고, 이벤트만 발행
+            musicEventProducer.publishMusicEvent(music.getSongName());
+            return existsMusic.get();
+
+        }
     }
+
+
+
 }
